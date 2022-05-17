d06a1504dbd11789fb3b3fed55f5de8084531d41
thephong2210 <thephong1412@gmail.com>
2022-05-17 08:54:49 +0700

    update qr code

--- phpqrcode/qrcode.php
+++ phpqrcode/qrcode.php
@@ -5,14 +5,15 @@ class qrcode1{
    // truyền vào url tạo qrcode
     public static function create_QRcode($url){
 
-        $path = 'images/';
-        $file = $path . uniqid() . ".png";
+        $path = __DIR__.'/images/';
+        $name_img = uniqid(). ".png";
+        $file = $path .  $name_img ;
         $ecc = "L";
         $pixel_Size = 10;
         $frame_Size = 10;
         
         QRcode::png($url, $file, $ecc, $pixel_Size, $frame_Size);
-        return   $file;
+        return   $name_img;
     }
    
 }
