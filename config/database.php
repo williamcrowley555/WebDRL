f639ec03bd62585fcb1b6692f92824fb0d063575
William <buiquanghuy.10122000.10@gmail.com>
2022-12-21 15:09:59 +0700

    Tracking config folder

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+    class Database{
+        private $host = "localhost";
+        private $port = "3307";
+        private $database_name = "diemrenluyen";
+        private $username = "root";
+        private $password = "";
+        public $conn;
+
+        public function getConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ":" . $this->port . ";dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+                //echo "Database connected";
+            }catch(PDOException $exception){
+                echo "Database could not be connected: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+
+     
+    }
+
+?>
